4ad7577fd074af1c0918234ecb962eee7883f7bc
Sammmpo <sampsa.kares@pp.inet.fi>
2017-05-28 00:10:00 +0300

    update

--- js/main.js
+++ js/main.js
@@ -1,24 +1,61 @@
 // The game mechanics
 
-var persons = [];
+var member = [];
 var raidSize = 10;
-function spawnPersons(){
-  for (i = 1; i <= raidSize; i++) {
-    persons[i] = {id:i, personality:1, dps:0, gear:50, spirit:50, time:50, available:1, inRaid:1};
-    for (j = 1; j <= 15; j++) {
-      var RNG = Math.floor((Math.random() * 3) + 1);
-      if (RNG == 1) { persons[i].gear += 10; }
-      if (RNG == 2) { persons[i].spirit += 10; }
-      if (RNG == 3) { persons[i].time += 10; }
-    }
-    persons[i].time = persons[i].time * 6;
-    persons[i].dps = persons[i].gear * persons[i].spirit;
-  }
-}
-spawnPersons();
 
+var playerCount = 0;
 var bossHealth = 1000000;
 var loot = 0;
+var rage = 0;
+var cheer = 0;
+
+class Player {
+  constructor(id, gear, spirit, time, active) {
+	this.id = id;
+	this.gear = gear;
+	this.spirit = spirit;
+	this.time = time;
+	this.active = active;
+  this.personality = 1;
+  }
+
+  get dps() {
+      return this.gear * this.spirit;
+  }
+
+}
+
+class Member {
+  constructor(id, gear, spirit, time, active) {
+	this.id = id;
+	this.gear = gear;
+	this.spirit = spirit;
+	this.time = time;
+	this.active = active;
+  this.personality = Math.floor((Math.random() * 2) + 2);
+
+  for (var j = 0; j < 15; j++) {
+	var rollStats = Math.floor((Math.random() * 3) + 1);
+      if (rollStats == 1) { this.gear += 10; }
+      if (rollStats == 2) { this.spirit += 10; }
+      if (rollStats == 3) { this.time += 60; }
+  }
+  }
+
+  get dps() {
+      if (rage == 1) {
+        return this.gear * this.spirit * 2;
+      } else { return this.gear * this.spirit; }
+  }
+
+}
+
+function spawnRaid(){
+  member.push(new Player(0, 100, 100, 1200, 1));
+  for (i = 1; i < raidSize; i++) {
+    member.push(new Member(i, 50, 50, 300, 1));
+  }
+}
 
 
 var startGameButton = document.getElementById("startGameButton");
@@ -26,6 +63,42 @@ startGameButton.addEventListener("click", startGame);
 var pullButton = document.getElementById("pullButton");
 pullButton.addEventListener("click", pull);
 
+
+var rageButton = document.querySelector("input[name=rageButton]");
+var rageInterval;
+rageButton.onchange = function() {
+if(this.checked) {
+    rage = 1;
+    rageInterval = window.setInterval(function(){
+      for (i = 1; i < raidSize; i++) {
+        if (member[i].personality !== 0) {
+          member[i].spirit -= 1;
+        }
+      }
+    }, 1000);
+  } else {
+    clearInterval(rageInterval);
+    rage = 0;
+  }
+}
+
+var cheerButton = document.querySelector("input[name=cheerButton]");
+var cheerInterval;
+cheerButton.onchange = function() {
+if(this.checked) {
+    cheer = 1;
+    cheerInterval = window.setInterval(function(){
+      for (i = 1; i < raidSize; i++) {
+        if (member[i].personality !== 0 && member[i].spirit < 100) {
+          member[i].spirit += 1;
+        }
+      }
+    }, 1000);
+  } else {
+    clearInterval(cheerInterval);
+    cheer = 0;
+  }
+}
 
 var reduceTimeButton = document.getElementById("reduceTimeButton");
 reduceTimeButton.addEventListener("click", gmReduceTime);
@@ -35,8 +108,14 @@ function startGame() {
   document.getElementById("startGameButton").disabled = true;
   pullButton.classList.remove("locked");
   document.getElementById("pullButton").disabled = false;
+  document.getElementById("cheerButton").disabled = false;
+  document.getElementById("rageButton").disabled = false;
+  spawnRaid();
   timer();
+  liveCheck();
+  updateGUI();
 }
+
 
 function pull() {
   dps();
@@ -45,30 +124,39 @@ function pull() {
 }
 
 function timer() {
-  window.setInterval(function(){
-    for (i = 1; i <= persons.length; i++) {
-      persons[i].time -= 1;
-      if (persons[i].time <= 0) {
-        personQuits(persons[i]);
-      }
+  var timeInterval = window.setInterval(function(){
+    for (i = 0; i < raidSize; i++) {
+      if (member[i].personality !== 0) { member[i].time -= 1; }
     }
   }, 1000);
 }
-function personQuits(quitter) {
-  quitter.personality = 0;
-  quitter.dps = 0;
-  quitter.gear = 0;
-  quitter.spirit = 0;
-  quitter.time = 0;
-  quitter.available = 0;
-  quitter.inRaid = 0;
+
+function liveCheck() {
+  var checkInterval = window.setInterval(function(){
+    if (member[0].time <= 0) { location.reload(); }
+    var count = 1;
+    for (i = 1; i < raidSize; i++) {
+      member[i].dps = member[i].gear * member[i].spirit;
+      if (member[i].time <= 0 || member[i].spirit <= 0) {
+        member[i].personality = 0;
+        member[i].gear = 0;
+        member[i].spirit = 0;
+        member[i].time = 0;
+        member[i].active = 0;
+      }
+      if (member[i].personality !== 0) { count++; }
+      playerCount = count;
+    }
+  }, 10);
 }
+
+
 function dps() {
   var dpsInterval = window.setInterval(function(){
-    if (bossHealth > 0) {
-      for (i = 1; i <= persons.length; i++) {
-          if (persons[i].available == 1 && persons[i].inRaid == 1) {
-            bossHealth -= persons[i].dps;
+    if (bossHealth > 0 && cheer == 0) {
+      for (i = 0; i < raidSize; i++) {
+          if (member[i].active == 1 && member[i].personality !== 0) {
+            bossHealth -= member[i].dps;
           }
           if (bossHealth < 0) { bossHealth = 0; }
       }
@@ -86,10 +174,7 @@ function dps() {
 
 
 function gmReduceTime() {
-  for (i = 1; i <= persons.length; i++) {
-    persons[i].time -= 100;
-    if (persons[i].time <= 0) {
-      personQuits(persons[i]);
-    }
+  for (i = 0; i < raidSize; i++) {
+      member[i].time -= 100;
   }
 }
--- js/update.js
+++ js/update.js
@@ -1,16 +1,25 @@
 // Updates to GUI
-
+function updateGUI(){
 
 // Update Table
-window.setInterval(function(){
-      for (i = 1; i <= raidSize; i++) {
-            document.getElementById("id"+i).innerHTML = persons[i].id;
-            document.getElementById("personality"+i).innerHTML = persons[i].personality;
-            document.getElementById("dps"+i).innerHTML = persons[i].dps;
-            document.getElementById("gear"+i).innerHTML = persons[i].gear;
-            document.getElementById("spirit"+i).innerHTML = persons[i].spirit;
-            document.getElementById("time"+i).innerHTML = persons[i].time;
-      }
-      document.getElementById("bossHealth").innerHTML = bossHealth;
-      document.getElementById("loot").innerHTML = loot;
+var raidTable = window.setInterval(function(){
+  document.getElementById("id-player").innerHTML = member[0].id;
+  document.getElementById("personality-player").innerHTML = member[0].personality;
+  document.getElementById("dps-player").innerHTML = member[0].dps;
+  document.getElementById("gear-player").innerHTML = member[0].gear;
+  document.getElementById("spirit-player").innerHTML = member[0].spirit;
+  document.getElementById("time-player").innerHTML = member[0].time;
+for (i = 1; i < raidSize; i++) {
+  document.getElementById("id"+i).innerHTML = member[i].id;
+  document.getElementById("personality"+i).innerHTML = member[i].personality;
+  document.getElementById("dps"+i).innerHTML = member[i].dps;
+  document.getElementById("gear"+i).innerHTML = member[i].gear;
+  document.getElementById("spirit"+i).innerHTML = member[i].spirit;
+  document.getElementById("time"+i).innerHTML = member[i].time;
+}
+  document.getElementById("playerCount").innerHTML = playerCount;
+  document.getElementById("bossHealth").innerHTML = bossHealth;
+  document.getElementById("loot").innerHTML = loot;
 }, 10);
+
+}
